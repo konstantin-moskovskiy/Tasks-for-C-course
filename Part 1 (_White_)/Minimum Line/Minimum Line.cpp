eb40a9acd20f44744642b9b0e0a558bfb59a61ad
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-05 00:07:11 +0300

    added first task

---
+++ Part 1 (_White_)/Minimum Line/Minimum Line.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	string a, b, c;
+
+	cin >> a >> b >> c;
+	if (a < b && a < c) {
+		cout << a;
+	}
+	else
+	{
+		if (b < c && b < a) {
+			cout << b;
+		}
+		else
+		{
+			cout << c;
+		}
+	}
+
+	return 0;
+}
