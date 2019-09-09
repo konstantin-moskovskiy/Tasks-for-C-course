009becf6441faca8c7c6e25560e75a032f260a82
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-09 21:18:31 +0300

    added task 'division'

---
+++ Part 1 (_White_)/Division/Division.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cin >> a >> b;
+	if (b != 0) {
+		cout << a / b;
+	}
+	else
+	{
+		cout << "Impossible";
+	}
+
+	return 0;
+}
