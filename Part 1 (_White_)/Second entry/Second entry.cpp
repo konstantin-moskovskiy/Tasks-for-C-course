efbb5f1e01264d03d9af5b1414a699cdd6cd83dd
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-10 23:14:08 +0300

    added task 'Second entry'

---
+++ Part 1 (_White_)/Second entry/Second entry.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	string f;
+	int count = 0, n, otvet = 0;
+
+	cin >> f;
+	for (int i = 0; i < f.size(); ++i) {
+		if (f[i] == 'f') {
+			count++;
+			if (count == 2) {
+				cout << i;
+				break;
+			}
+		}
+	}
+	if (count == 0) {
+		cout << -2;
+	}
+	else
+	{
+		if (count == 1) {
+			cout << -1;
+		}
+	}
+}
