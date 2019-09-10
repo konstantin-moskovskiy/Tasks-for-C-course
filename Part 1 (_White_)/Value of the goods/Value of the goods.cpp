89649ab1fc4dcbe13f3d3052db30d6217d7d3161
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-10 18:26:38 +0300

    added task 'Value of the goods'

---
+++ Part 1 (_White_)/Value of the goods/Value of the goods.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	double ishodnaya_stoimost, A, B, skidka_A, skidka_B;
+
+	cin >> ishodnaya_stoimost >> A >> B >> skidka_A >> skidka_B;
+
+	if (ishodnaya_stoimost > A) {
+		if (ishodnaya_stoimost > B) {
+			cout << ishodnaya_stoimost - ishodnaya_stoimost * skidka_B / 100;
+		}
+		else
+		{
+			cout << ishodnaya_stoimost - ishodnaya_stoimost * skidka_A / 100;
+		}
+	}
+	else {
+		cout << ishodnaya_stoimost;
+	}
+}
