4f28847052ca4a4d7f310a73e8f10d6b4e01f175
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-10 23:47:57 +0300

    added task 'Binary numbers'

---
+++ Part 1 (_White_)/Binary numbers/Binary numbers.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector <int> result = {};
+	int n;
+
+	cin >> n;
+	while (n > 0) {
+		if (n % 2 == 0) {
+			result.push_back(0);
+		}
+		else {
+			result.push_back(1);
+		}
+		n /= 2;
+	}
+	for (int i = result.size() - 1; i >= 0; --i) {
+		cout << result[i];
+	}
+}
