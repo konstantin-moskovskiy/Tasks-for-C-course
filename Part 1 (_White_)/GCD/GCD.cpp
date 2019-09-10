ecad6819c212f9edbea907a66a818e845b64495a
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-10 23:28:04 +0300

    added task 'GCD'

---
+++ Part 1 (_White_)/GCD/GCD.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+
+	cin >> A >> B;
+	while (A > 0 && B > 0) {
+		if (A > B) {
+			A %= B;
+		}
+		else {
+			B %= A;
+		}
+	}
+	cout << A + B;
+
+	return 0;
+}
