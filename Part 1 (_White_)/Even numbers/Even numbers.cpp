9d23bb2373c7078d95900ca1b25321b9b2f83b4d
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-10 22:19:34 +0300

    added task 'Even numbers'

---
+++ Part 1 (_White_)/Even numbers/Even numbers.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+
+	cin >> A >> B;
+	for (A; A <= B; ++A) {
+		if (A % 2 == 0) {
+			cout << A << " ";
+		}
+	}
+
+	return 0;
+}
