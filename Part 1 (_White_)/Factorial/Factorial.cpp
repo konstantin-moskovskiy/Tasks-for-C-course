c6a81d88e370e021184fccb3a50dff28c04f4236
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-15 23:43:16 +0300

    added task 'Factorial' and description

---
+++ Part 1 (_White_)/Factorial/Factorial.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int Factorial(int x) {
+	int result = 1;
+
+	if (x <= 0) {
+		return 1;
+	}
+	else {
+		for (int i = 1; i <= x; ++i) {
+			result *= i;
+		}
+	}
+
+	return result;
+}
+
+int main() {
+	int a;
+
+	cin >> a;
+	cout << Factorial(a);
+
+	return 0;
+}
