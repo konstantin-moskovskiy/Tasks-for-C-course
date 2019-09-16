b54c46be07d5de08639750c29d70ad692de12b23
Konstantin Moskovskiy <mskou1999@mail.ru>
2019-09-16 21:51:29 +0300

    added task 'Selection of palindromes' and description

---
+++ Part 1 (_White_)/Selection of palindromes/Selection of palindromes.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool IsPalindrom(string s) {
+	for (int i = 0; i < s.size() / 2; ++i) {
+		if (s[i] != s[s.size() - i - 1]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void PalindromFilter(vector<string> words, int minLength) {
+	for (auto s : words) {
+		if (IsPalindrom(s) && s.size() >= minLength) {
+		cout << s << endl;
+		}
+	}
+}
